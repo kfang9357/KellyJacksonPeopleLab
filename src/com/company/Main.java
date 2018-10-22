3f059b57a5e4b90891caa082bfe8d08b73fadf80
BT_1N3_27 <kfang9357@bths.edu>
2018-10-22 08:42:37 -0400

    student class made

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+    public static String [] firstNames = ("Jackson"+"Kelly"+"Yan"+"Nelsom");
+    public static String [] familyNames = "Thunder"+"Rain"+"River"+"Oak");
+
 }
