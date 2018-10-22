6411c4c3afaabf305eebcc0d9599d5d4e5c479e6
BT_1N3_28 <jacksonjjacobs@gmail.com>
2018-10-22 08:43:03 -0400

    Teacher class

---
+++ src/com/company/Teacher.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Teacher
+{
+    //fields
+    private String subject, title, familyName;
+    public Person(String subject, String title, String familyName)
+    {
+        this.subject = subject;
+        this.title = title;
+        this.familyName = familyName;
+    }
+    public abstract String toString();
+    public String getSubject()
+    {
+        return subject;
+    }
+    public String
+
+}
