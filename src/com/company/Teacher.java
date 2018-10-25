fda7786a647c740819ad0ea63a1e354ce05bbe50
BT_1N3_28 <jacksonjjacobs@gmail.com>
2018-10-25 08:41:47 -0400

    updated teacher class

--- src/com/company/Teacher.java
+++ src/com/company/Teacher.java
@@ -3,18 +3,38 @@ package com.company;
 public class Teacher
 {
     //fields
-    private String subject, title, familyName;
-    public Person(String subject, String title, String familyName)
+    private String subject;
+    private String title;
+    private String familyName;
+    //constructors
+    public Teacher(String subject, String title, String familyName)
     {
         this.subject = subject;
         this.title = title;
         this.familyName = familyName;
     }
-    public abstract String toString();
-    public String getSubject()
-    {
+    //getters and setters
+    public String getSubject() {
         return subject;
     }
-    public String
 
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
